c3c22d7c1e58e8cd60ac5edc4c285e52cecb649d
ridingproject <kk91311@gmail.com>
2015-08-16 18:59:43 +0900

    test

--- src/sample/cycleclub/club/web/CycleClubServlet.java
+++ src/sample/cycleclub/club/web/CycleClubServlet.java
@@ -37,7 +37,7 @@ public class CycleClubServlet extends HttpServlet {
 			// 모임 수정
 			request.setAttribute("next", "update");
 			String ccodestr = request.getParameter("ccode");
-			System.out.println(ccodestr); //detail에서 ccode넘어옴
+			//System.out.println(ccodestr); //detail에서 ccode넘어옴
 			int ccode = Integer.parseInt(ccodestr);
 			ClubVO cvo = service.selectClub(ccode);
 			request.setAttribute("cvo", cvo); //cvo넘겨줌
@@ -79,7 +79,6 @@ public class CycleClubServlet extends HttpServlet {
 			request.setAttribute("cvo", cvo);
 			ArrayList<MemberVO> mlist = service.joinClubList();
 			request.setAttribute("mlist", mlist);
-			System.out.println("detail 겠!!!!!!");
 			request.setAttribute("next", "update");
 			url = "/WEB-INF/cycleclub/club/detailClub.jsp";
 		}else if("join".equals(action)){
